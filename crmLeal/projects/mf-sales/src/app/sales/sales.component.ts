8ea70ce8cb9be6cd5b15396c1c2c354fb3fbd34a
Rigock <rigock11@gmail.com>
2023-12-04 15:29:48 -0500

    clean code

--- crmLeal/projects/mf-sales/src/app/sales/sales.component.ts
+++ crmLeal/projects/mf-sales/src/app/sales/sales.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { SalesService } from '../services/sales.service';
-import { single } from '../services/data';
 
 @Component({
   selector: 'app-sales',
